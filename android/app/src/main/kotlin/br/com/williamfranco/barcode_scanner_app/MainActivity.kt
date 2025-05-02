b6e8744f58f73340c95e96da1b26f5dd69c2903f
William Franco <williamff@outlook.com>
2025-05-02 19:11:42 -0300

    :building_construction: Update project architecture.

--- android/app/src/main/kotlin/br/com/williamfranco/barcode_scanner_app/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/williamfranco/barcode_scanner_app/MainActivity.kt
@@ -2,4 +2,4 @@ package br.com.williamfranco.barcode_scanner_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()

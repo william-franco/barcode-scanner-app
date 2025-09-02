1729b5091f56a3a18e6e52520c28ea7ab82c19b2
William Franco <williamff@outlook.com>
2025-09-02 15:39:35 -0300

    :arrow_up: Updated dependencies.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "br.com.williamfranco.barcode_scanner_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.2.12479018"
+    ndkVersion = "27.3.13750724"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
